86c5d4df34d32acf48eb4b7bbad0e863f7fb0afe
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2023-12-29 08:41:59 +0100

    Add missing hint

--- src/main/java/com/example/demo/Sb38941Application.java
+++ src/main/java/com/example/demo/Sb38941Application.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import java.util.Collections;
+
+import com.example.demo.Sb38941Application.Sb38941ApplicationRuntimeHints;
+import liquibase.changelog.ChangeLogHistoryServiceFactory;
+
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(Sb38941ApplicationRuntimeHints.class)
 public class Sb38941Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb38941Application.class, args);
+	}
+
+	static class Sb38941ApplicationRuntimeHints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.reflection().registerType(ChangeLogHistoryServiceFactory.class, (type) ->
+					type.withConstructor(Collections.emptyList(), ExecutableMode.INVOKE));
+		}
 	}
 
 }
